b0e2c299ade3b90a62fe9fe73b4b6bcdf6183d32
Badjessa Bahoumda <bbb219@lehigh.edu>
2016-10-20 02:44:53 +0000

    update hw07

--- CSE2/hw07/Triangles.java
+++ CSE2/hw07/Triangles.java
@@ -6,24 +6,29 @@ public class Triangles{
     //add main method
     public static void main(String[] args){
         //defining variable for while loop
-        int X = 0;
+        int x = 0;
         int y =0; 
         Scanner myScan = new Scanner(System.in);
-
-                while (X == 0) { //enter loop if condition is met
-                        System.out.print("Enter a number between 5 and 30: "); //print statement if(input.hasNextInt()) { //if an int was typed in enter if statement
-                        y = myScan.nextInt(); //assigning variable
-                        X = 1; //exit the loop
-                        if (y >= 5 && y <= 30) { //if y is between those numbers do nothing
-                    }
-                        else{ //if does not meet those condidtions 
-                        System.out.println("Please Enter a number between 5 and 30."); 
-                        X = 0; //print statement and reenter loop
-                    }
+            while (x == 0){
+                System.out.print("Input an Integer between 5 and 30: ");
+                while(!myScan.hasNextInt()){
+                    System.out.println("Error: This is not an integer");
+                    myScan.next();
                 }
-                        if (!myScan.hasNextInt()) { //if an int is not entered 
-                        System.out.println("Please Enter an integer."); 
-                        myScan.next(); //print statement and re enter loop
+                x = myScan.nextInt();
+                if (x < 5 || x > 30){
+                    System.out.println("Error: input an integer between 5 and 30");
+                    x = 0;
                 }
         }
+        
+                for (int i = 1; i<=x; i++){
+                    System.out.print(+i); 
+                    for (int k = x; k>=i; k--){
+                        System.out.print(+k); 
+                        System.out.println();
+                    }
+                    System.out.println();
+                }
+    }         
 }
